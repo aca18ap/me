3e5b6003eb63e8f88f163ddd143f8627857daaf4
Alberto Pia <apia1@sheffield.ac.uk>
2021-10-16 22:01:31 +0100

    aa

--- js.js
+++ js.js
@@ -2,10 +2,7 @@ window.onload = function() {
     let today = new Date();
     let time = today.getHours();
     let greeting;
-<<<<<<< HEAD
     console.log(time)
-=======
->>>>>>> 5295fbd8be9d1cb4359b818a9c960b08116b5535
     if (time > 4 && time < 12) {
         greeting = 'Good Morning Human!'
     } else if (time > 12 && time < 19) {
