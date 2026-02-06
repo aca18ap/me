f96a004ad8a74cbd9a9320471796f79bf6b1e4ce
Alberto Pia <albi.pia99@gmail.com>
2026-02-06 17:19:52 +0000

    Changed index terminal text

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,6 @@
 const CONTENT = `Hello! I'm Alberto Pia, welcome to my personal website!
 I'm a software engineer with a passion for aviation, music, and photography, among other things.
 I hope to share some of my interests with you through this website! Thank you for visiting!
-Type in help to see the available commands.
 `
 
 async function introduction() {
